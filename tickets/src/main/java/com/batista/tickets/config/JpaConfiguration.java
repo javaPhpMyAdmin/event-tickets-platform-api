ddfa6b5302146ad33bd8b3614f2cfc8f2734ff78
JavaPhpMyAdmin <chelobat16411@gmail.com>
2025-09-19 20:35:28 -0300

    feat: jpa configuration and orm.xml for auditing

--- tickets/src/main/java/com/batista/tickets/config/JpaConfiguration.java
+++ tickets/src/main/java/com/batista/tickets/config/JpaConfiguration.java
@@ -1,5 +1,10 @@
 package com.batista.tickets.config;
 
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Configuration
+@EnableJpaAuditing
 public class JpaConfiguration {
 
 }
